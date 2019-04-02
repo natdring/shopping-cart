be371ab4d78b44ea24e65113e6d4e6d0609fbc94
Nat <natdring97@gmail.com>
2019-04-01 17:43:13 -0700

    added display all button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sqlite3
 import json
 from flask import Flask, jsonify, render_template, request
@@ -12,15 +13,19 @@ def main():
 @app.route('/test', methods = ['POST'])
 def test():
     message = request.get_json()['message']
+    regex = re.match("^([a-z]|[A-Z]|[0-9])*$", message)
+    if regex != None:
+        pass
 
     with sqlite3.connect('database.db') as conn:
         cur = conn.cursor()
-        cur.execute('SELECT * FROM products WHERE name=?;', (message,))
+        if message == "show all":
+            cur.execute('SELECT * FROM products;')
+
+        else:
+            cur.execute('SELECT * FROM products WHERE name=?;', (message,))
         rows = cur.fetchall()
         render_template('index.html', rows=rows)
-        print(rows)
-        for row in rows:
-            print(row)
         conn.commit()
         return jsonify({'msg':rows})
     
--- static/script.js
+++ static/script.js
@@ -15,7 +15,7 @@ let getJSON = function (url, params, callback) {
     xhr.send(params);
 };
 
-function clicked(){
+function searchClicked(){
     let query = document.getElementById('search').value;
     let params = JSON.stringify({message:query});
 
@@ -24,6 +24,49 @@ function clicked(){
             console.log("error retrieving data" + err)
         }
         else{
+            var x = document.getElementById("myTable").rows.length;
+            for(var i=x-1; i > 0; i-=1) {
+                document.getElementById("myTable").deleteRow(i);
+            } 
+            var items = data['msg']
+             cols = document.querySelector("#table > table > thead > tr").children.length, // Get length of the columns in thead
+             tbody = document.querySelector("#table > table > tbody");
+             for (var i = 0; i < items.length; i += 1) {
+                var row = tbody.insertRow(i);
+                for (var j = 0; j < cols; j += 1) {
+                  var cell = row.insertCell(j);
+                  if (j === 0) { // First td
+                    var id = document.createTextNode(items[i][0]);
+                    cell.appendChild(id);
+                  }
+                  if (j === 1) { // Second td
+                    var name = document.createTextNode(items[i][1]);
+                    cell.appendChild(name);
+                  }
+                  if (j === 2) { // Third td
+                    var price = document.createTextNode(items[i][2]);
+                    cell.appendChild(price);
+                  }
+                }
+              }
+            console.log(data['msg'])
+        }
+    });
+}
+
+function allClicked(){
+    let query = 'show all';
+    let params = JSON.stringify({message:query});
+
+    getJSON('/test', params, function(err, data){
+        if(err !== null){
+            console.log("error retrieving data" + err)
+        }
+        else{
+            var x = document.getElementById("myTable").rows.length;
+            for(var i=x-1; i > 0; i-=1) {
+                document.getElementById("myTable").deleteRow(i);
+            } 
             var items = data['msg']
              cols = document.querySelector("#table > table > thead > tr").children.length, // Get length of the columns in thead
              tbody = document.querySelector("#table > table > tbody");
